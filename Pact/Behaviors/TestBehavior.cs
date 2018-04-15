0e66b2b2daf316e92b755ef98d0988af16721087
Nicholas Anderson <nickatog@gmail.com>
2018-04-15 17:53:39 -0500

    Toying with ideas for the UI for editing deck titles.

--- Pact/Behaviors/TestBehavior.cs
+++ Pact/Behaviors/TestBehavior.cs
@@ -3,13 +3,41 @@ using System.Windows.Controls;
 
 namespace Pact.Behaviors
 {
-    public static class ToggleVisibilityBehavior
+    public static class CollapseOnClickBehavior
     {
         public static readonly DependencyProperty ToggleButton =
             DependencyProperty.RegisterAttached(
                 "ToggleButton",
                 typeof(Button),
-                typeof(ToggleVisibilityBehavior),
+                typeof(CollapseOnClickBehavior),
+                new UIPropertyMetadata(null, OnToggleButtonChanged));
+
+        public static Button GetToggleButton(UIElement element)
+        {
+            return (Button)element.GetValue(ToggleButton);
+        }
+
+        private static void OnToggleButtonChanged(DependencyObject @object, DependencyPropertyChangedEventArgs args)
+        {
+            if (@object is UIElement element)
+            {
+                Button button = GetToggleButton(element);
+                if (button == null)
+                    return;
+                
+                button.Click +=
+                    (__1, __2) => Panel.SetZIndex(element, -1);
+            }
+        }
+    }
+
+    public static class ShowOnClickBehavior
+    {
+        public static readonly DependencyProperty ToggleButton =
+            DependencyProperty.RegisterAttached(
+                "ToggleButton",
+                typeof(Button),
+                typeof(ShowOnClickBehavior),
                 new UIPropertyMetadata(null, OnToggleButtonChanged));
 
         public static Button GetToggleButton(UIElement element)
@@ -26,11 +54,93 @@ namespace Pact.Behaviors
                     return;
 
                 button.Click +=
+                    (__1, __2) => Panel.SetZIndex(element, 1);
+            }
+        }
+    }
+
+    public static class SetVisibleOnClickBehavior
+    {
+        public static readonly DependencyProperty ToggleButton =
+            DependencyProperty.RegisterAttached(
+                "ToggleButton",
+                typeof(Button),
+                typeof(SetVisibleOnClickBehavior),
+                new UIPropertyMetadata(null, OnToggleButtonChanged));
+
+        public static Button GetToggleButton(UIElement element)
+        {
+            return (Button)element.GetValue(ToggleButton);
+        }
+
+        private static void OnToggleButtonChanged(DependencyObject @object, DependencyPropertyChangedEventArgs args)
+        {
+            if (@object is UIElement element)
+            {
+                Button button = GetToggleButton(element);
+                if (button == null)
+                    return;
+
+                button.Click += (__1, __2) => element.Visibility = Visibility.Visible;
+            }
+        }
+    }
+
+    public static class SetCollapsedOnClickBehavior
+    {
+        public static readonly DependencyProperty ToggleButton =
+            DependencyProperty.RegisterAttached(
+                "ToggleButton",
+                typeof(Button),
+                typeof(SetCollapsedOnClickBehavior),
+                new UIPropertyMetadata(null, OnToggleButtonChanged));
+
+        public static Button GetToggleButton(UIElement element)
+        {
+            return (Button)element.GetValue(ToggleButton);
+        }
+
+        private static void OnToggleButtonChanged(DependencyObject @object, DependencyPropertyChangedEventArgs args)
+        {
+            if (@object is UIElement element)
+            {
+                Button button = GetToggleButton(element);
+                if (button == null)
+                    return;
+
+                button.Click += (__1, __2) => element.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+
+    public static class SelectAllOnClickBehavior
+    {
+        public static readonly DependencyProperty ToggleButton =
+            DependencyProperty.RegisterAttached(
+                "ToggleButton",
+                typeof(Button),
+                typeof(SelectAllOnClickBehavior),
+                new UIPropertyMetadata(null, OnToggleButtonChanged));
+
+        public static Button GetToggleButton(UIElement element)
+        {
+            return (Button)element.GetValue(ToggleButton);
+        }
+
+        private static void OnToggleButtonChanged(DependencyObject @object, DependencyPropertyChangedEventArgs args)
+        {
+            if (@object is TextBox textBox)
+            {
+                Button button = GetToggleButton(textBox);
+                if (button == null)
+                    return;
+
+                button.Click +=
                     (__1, __2) =>
-                        element.Visibility =
-                            element.Visibility == Visibility.Visible
-                            ? Visibility.Collapsed
-                            : Visibility.Visible;
+                    {
+                        textBox.Focus();
+                        textBox.SelectAll();
+                    };
             }
         }
     }
