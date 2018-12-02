9ca31bd33a48ae7ac3d4597f2996c6bb648c9e6e
Nicholas Anderson <nickatog@gmail.com>
2018-12-01 22:25:43 -0600

    Messing around with custom window chrome.

---
+++ Pact/UI/Properties/AppClose.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Pact.Properties
+{
+    public static class AppClose
+    {
+        public static readonly DependencyProperty Enable =
+            DependencyProperty.RegisterAttached(
+                "Enable",
+                typeof(bool),
+                typeof(AppClose),
+                new UIPropertyMetadata(false, OnEnableChanged));
+
+        public static bool GetEnable(
+            UIElement element)
+        {
+            return (bool?)element?.GetValue(Enable) ?? false;
+        }
+
+        private static void OnEnableChanged(
+            DependencyObject @object,
+            DependencyPropertyChangedEventArgs args)
+        {
+            if (@object is Button button)
+            {
+                bool enabled = GetEnable(button);
+                if (!enabled)
+                    return;
+
+                button.Click +=
+                    (__1, __2) => Application.Current.Shutdown();
+            }
+        }
+    }
+}
